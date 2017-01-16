4e67e03db0d99f868ffa6f42ba6767cf4117c981
keiraeleison <keiraeleison@gmail.com>
2017-01-16 10:44:53 +0100

    Added dummy POST, PUT, and DELETE requests

---
+++ README.md
@@ -0,0 +1,14 @@
+Start application with:
+-------------------------
+    npm run start // in production mode
+    npm run dev   // in developement mode
+
+You can easily test out the different API endpoint by:
+  * Installing Postman extension for Chrome
+  * Importing the 'node-rest-api.postman_collection.json' collection into it
+  * Looking through and running the predifined requests
+
+Packages used: 
+  * express
+  * body-parser
+  * cors // cross origin resourse sharing
--- server.js
+++ server.js
@@ -30,6 +30,51 @@ app.get('/api/contacts/:id', (req, res) => {
   res.json(contact[0]);
 });
 
+app.post('/api/contacts', (req, res) => {
+  let contact = {
+    id: contacts.length + 1,
+    first_name: req.body.firstname,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    website: req.body.website
+  }
+
+  contacts.push(contact);
+  res.json(contact);
+});
+
+app.put('/api/contacts/:id', (req, res) => {
+  if (!contacts) {
+    res.status(404).json({ message: 'No contacts found' });
+  }
+  const requestId = req.params.id;
+  let contact = contacts.filter(contact => {
+    return contact.id == requestId;
+  })[0];
+
+  const index = contacts.indexOf(contact);
+  const key = Object.keys(req.body);
+  key.forEach(key => {
+    contact[key] = req.body[key];
+  });
+
+  contacts[index] = contact;
+  res.json(contact);
+});
+
+app.delete('/api/contacts/:id', (req, res) => {
+  const requestId = req.params.id;
+
+  let contact = contacts.filter(contact => {
+    return contact.id == requestId;
+  })[0];
+
+  const index = contacts.indexOf(contact);
+
+  contacts.splice(index, 1); // take only 1 position out of the array
+  res.json( {message: `User with ${requestId} has been deleted`} );
+});
+
 
 app.listen(port, hostname, () => {
   console.log(`Server us running at http://${hostname}:${port}`);
