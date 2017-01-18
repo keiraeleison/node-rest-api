b3136978b329b3e1ad2621bec356495451bfc7c5
Kira <keiraeleison@gmail.com>
2017-01-18 21:13:29 +0100

    Update Contact.js

--- api/contacts/model/Contact.js
+++ api/contacts/model/Contact.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ContactSchema = new Schema({
-  first_name: { type: String, rquired: true },
-  last_name: { type: String, rquired: true },
-  email: { type: String, rquired: true },
-  website: { type: String, rquired: true }
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true },
+  website: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Contact', ContactSchema);
